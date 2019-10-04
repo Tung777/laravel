71b1a3a35b4ede190fdc3f1356d99e73ff5d1d2a
Tung777 <49684861+Tung777@users.noreply.github.com>
2019-10-04 23:33:59 +0700

    D:\github\laravel\bt646\database\migrations\2014_10_12_100000_create_password_resets_table.php

--- bt646/routes/hotel.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::group(['prefix' => '/hotel'], function () {
-    Route::get('/view', 'HotelController@showAll')->name('showAll');
-
-    Route::get('/detail', 'HotelController@showDetail')->name('showDetail');
-});
